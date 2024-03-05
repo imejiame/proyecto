73c1bb9e2cd40079bdbca322945f3e65c0aaccea
imejiame <imejiame8@gmail.com.>
2024-03-05 05:45:51 -0500

    new 5-3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './vistas/login/login.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/vistas/login/login.component.ts
+++ src/app/vistas/login/login.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { parseTemplate } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
 
+export class LoginComponent implements OnInit {
+ 
+  formLogin = new FormGroup({
+    Usuario: new FormControl ('',Validators.required) ,
+    password: new FormControl ('',Validators.required)
+  });
+
+  ngOnInit(): void {
+    
+  }
+
+  OnLogin(form:any){
+    console.log(form);
+  }
 }
